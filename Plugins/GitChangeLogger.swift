d287ae632d0625c339e4f20c6883a13db8c11d51
Igor Vasilev <ivasilev@productiveedge.com>
2024-06-09 09:13:37 +0200

    CommandPlugin support

--- Plugins/GitChangeLogger.swift
+++ Plugins/GitChangeLogger.swift
@@ -7,16 +7,36 @@ struct GitChangeLogger: XcodeCommandPlugin {
 
     func performCommand(context: XcodeProjectPlugin.XcodePluginContext, arguments: [String]) throws {
 
+        let directory = context.pluginWorkDirectory
+        let tool = try context.tool(named: "GitChangeLogExtractor")
+        try run(workDirectory: directory, tool: tool)
+    }
+}
+
+extension GitChangeLogger: CommandPlugin {
+    
+    func performCommand(context: PackagePlugin.PluginContext, arguments: [String]) async throws {
+
+        let directory = context.pluginWorkDirectory
+        let tool = try context.tool(named: "GitChangeLogExtractor")
+        try run(workDirectory: directory, tool: tool)
+    }
+}
+
+fileprivate extension GitChangeLogger {
+
+    func run(workDirectory: PackagePlugin.Path, tool: PluginContext.Tool) throws {
+
         let baseBranch = "develop"
         let targetBranch = "delayed/SFFEAT0010489-landing-redesign"
 
-        let workDirectory = context.pluginWorkDirectory
+
         let output = workDirectory.appending(subpath: "changelog/changelog.md")
 
-        let extractorTool = try context.tool(named: "GitChangeLogExtractor")
+
 
         let extractor = Process()
-        extractor.executableURL = URL(fileURLWithPath: extractorTool.path.string)
+        extractor.executableURL = URL(fileURLWithPath: tool.path.string)
         extractor.arguments = ["--base", baseBranch,
                          "--target", targetBranch,
                          "--output", output.string]
